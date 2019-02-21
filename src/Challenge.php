44ec0842b8ecd46742c6edd542204f7acbeeea9f
Jose Manuel Robles <das.haas@gmail.com>
2019-02-21 08:52:47 -0500

    Update Challenge.php

--- src/Challenge.php
+++ src/Challenge.php
@@ -4,46 +4,13 @@ namespace App;
 
 class Challenge
 {
-
-    // array with the answers
-    private $fizzbuzz = array(3=>"fizz",6=>"fizz",9=>"fizz",12=>"fizz",15=>"fizz",18=>"fizz",21=>"fizz",24=>"fizz",27=>"fizz",30=>"fizz",33=>"fizz",36=>"fizz",39=>"fizz",42=>"fizz",45=>"fizz",48=>"fizz",51=>"fizz",54=>"fizz",57=>"fizz",60=>"fizz",63=>"fizz",66=>"fizz",69=>"fizz",72=>"fizz",75=>"fizz",78=>"fizz",81=>"fizz",84=>"fizz",87=>"fizz",90=>"fizz",93=>"fizz",96=>"fizz",99=>"fizz",5=>"buzz",10=>"buzz",15=>"buzz",20=>"buzz",25=>"buzz",30=>"buzz",35=>"buzz",40=>"buzz",45=>"buzz",50=>"buzz",55=>"buzz",60=>"buzz",65=>"buzz",70=>"buzz",75=>"buzz",80=>"buzz",85=>"buzz",90=>"buzz",95=>"buzz",100=>"buzz",15=>"fizzbuzz",30=>"fizzbuzz",45=>"fizzbuzz",60=>"fizzbuzz",75=>"fizzbuzz",90=>"fizzbuzz");
-
     public function output()
     {
     	// Range through the 100 numbers
 	foreach(range(1,100) as $i) {	
-	    echo $this->fizzbuzz($i);
+	    //echo $this->fizzbuzz($i);
 	    echo "\n";
 	}
         return null;
     }
-
-	// Working version but does not meet the requirements.
-	private function _fizzBuzz($i) 
-	{
-	    // If 3 or 5 output FizzBuzz
-	    if ($i % 3 == 0 && $i % 5 == 0) {
-	        return "FizzBuzz";
-	    } elseif ($i % 3 == 0) {
-	        return "Fizz";
-	    } elseif ($i % 5 == 0) {
-	        return "Buzz";
-	    } else {
-	        return $i;	
-	    }
-	}
-
-	// New solution that meets the constraints but not in a glamorous fashion :)
-	private function fizzBuzz($i) 
-	{
-	    // Generally - it's not a great idea to supress warnings but for this challenge I think this will do.
-	    @$answer = $this->fizzbuzz[$i];
-
-	    if (!isset($answer)) {
-	        $answer = $i;	
-	    }
-
-	    return $answer;
-	}
-
 }
